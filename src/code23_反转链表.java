6bceb9a9d785a897b0407adaede73c9e4dda358a
ZTL123Z <2746395413@qq.com>
2026-03-30 23:26:27 +0800

    code23_反转链表

---
+++ src/code23_反转链表.java
@@ -0,0 +1,22 @@
+public class code23_反转链表 {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}

206dbf6354e25ec0e1951a60a5e4a54485693475
ZTL123Z <2746395413@qq.com>
2026-04-02 19:06:55 +0800

    code27_合并两个有序链表

---
+++ src/code27_合并两个有序链表.java
@@ -0,0 +1,25 @@
+public class code27_合并两个有序链表 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (list1 != null || list2 != null) {
+            if (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    cur.next = list1;
+                    list1 = list1.next;
+                } else {
+                    cur.next = list2;
+                    list2 = list2.next;
+                }
+            } else if (list1 != null) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        return head.next;
+    }
+}
